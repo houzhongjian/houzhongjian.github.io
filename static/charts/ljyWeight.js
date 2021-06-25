6227d4c93f97ccbe5f1595b2407627375aace891
liujingying <jingying.liu1@pactera-fintech.com>
2021-06-25 16:15:00 +0800

    1

---
+++ static/charts/ljyWeight.js
@@ -0,0 +1,90 @@
+var data = {
+    "weightData":[
+        {
+            "changeDate": "2021-06-05",
+            "weight": "62.10"
+        },
+        {
+            "changeDate": "2021-06-06",
+            "weight": "61.80"
+        },
+        {
+            "changeDate": "2021-06-07",
+            "weight": "61.90"
+        },
+        {
+            "changeDate": "2021-06-08",
+            "weight": "62.20"
+        },
+        {
+            "changeDate": "2021-06-09",
+            "weight": "61.90"
+        },
+        {
+            "changeDate": "2021-06-10",
+            "weight": "61.60"
+        },
+        {
+            "changeDate": "2021-06-11",
+            "weight": "61.50"
+        },
+        {
+            "changeDate": "2021-06-12",
+            "weight": "61.20"
+        },
+        {
+            "changeDate": "2021-06-13",
+            "weight": "60.80"
+        },
+        {
+            "changeDate": "2021-06-14",
+            "weight": "61.30"
+        },
+        {
+            "changeDate": "2021-06-15",
+            "weight": "61.10"
+        },
+        {
+            "changeDate": "2021-06-16",
+            "weight": "61.50"
+        },
+        {
+            "changeDate": "2021-06-17",
+            "weight": "61.20"
+        },
+        {
+            "changeDate": "2021-06-18",
+            "weight": "61.05"
+        },
+        {
+            "changeDate": "2021-06-19",
+            "weight": "60.65"
+        },
+        {
+            "changeDate": "2021-06-20",
+            "weight": "60.45"
+        },
+        {
+            "changeDate": "2021-06-21",
+            "weight": "60.95"
+        },
+        {
+            "changeDate": "2021-06-22",
+            "weight": "61.05"
+        },
+        {
+            "changeDate": "2021-06-23",
+            "weight": "60.65"
+        },
+        {
+            "changeDate": "2021-06-24",
+            "weight": "60.60"
+        },
+        {
+            "changeDate": "2021-06-25",
+            "weight": "60.85"
+        },
+       
+    ]
+
+}
